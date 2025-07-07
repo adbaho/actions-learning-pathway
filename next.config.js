50cd7f5969546ebc0e5305997a69acea4d7346fb
adbaho <holger.barf@siemens.com>
2025-07-07 09:15:47 +0200

    play with lint

--- next.config.js
+++ next.config.js
@@ -5,8 +5,26 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+    
+  },
   
   reactStrictMode: true,
+  
 }
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
 module.exports = nextConfig
