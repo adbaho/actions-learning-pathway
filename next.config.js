765fd2f932df87d8693dddbf2f20e081f69b7096
adbaho <holger.barf@siemens.com>
2025-07-07 09:18:21 +0200

    once again

--- next.config.js
+++ next.config.js
@@ -17,14 +17,4 @@ const nextConfig = {
   
 }
 
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next'],
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  }),
-]
-
 module.exports = nextConfig
